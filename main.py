d5ddbd2c48c2186819cd64da3761de912fc4a622
Zitm <zitonovdmitriy@gmail.com>
2023-11-27 13:15:12 +0400

    добавление конструктора словаря pydantic

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import os
+from pydantic import BaseModel
+
+
+
 
 d=input("vvod ")
 
@@ -15,3 +19,17 @@ for filename in os.listdir(d):
                #тут будет функция перебора списка и проверки на натуральность 
                print(temp)
         print(temp)
+
+
+class Dictionary(BaseModel):
+    простое: bool
+    вхождений: int
+    делители: list
+
+def constr(n,p,o,m):
+    temp=Dictionary(простое=p,вхождений=o,делители=m)
+    return {str(n):temp.model_dump()}
+
+print(constr(30,True,5,[1,2,3,4,54]))
+
+
